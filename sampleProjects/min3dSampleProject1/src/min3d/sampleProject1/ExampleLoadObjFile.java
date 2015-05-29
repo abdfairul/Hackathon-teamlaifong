cdf34c26c0acca6463d2f1554779dfa7d3d2727a
abdfairul <abdfairul@hotmail.com>
2015-05-29 18:28:55 +0800

    obj load face

--- sampleProjects/min3dSampleProject1/src/min3d/sampleProject1/ExampleLoadObjFile.java
+++ sampleProjects/min3dSampleProject1/src/min3d/sampleProject1/ExampleLoadObjFile.java
@@ -21,17 +21,16 @@ public class ExampleLoadObjFile extends RendererActivity {
 		scene.lights().add(new Light());
 		
 		IParser parser = Parser.createParser(Parser.Type.OBJ,
-				getResources(), "min3d.sampleProject1:raw/camaro_obj", true);
+				getResources(), "min3d.sampleProject1:raw/face_obj", true);
 		parser.parse();
 
 		objModel = parser.getParsedObject();
-		objModel.scale().x = objModel.scale().y = objModel.scale().z = .7f;
+		objModel.scale().x = objModel.scale().y = objModel.scale().z = .01f;
 		scene.addChild(objModel);
 	}
 
 	@Override
 	public void updateScene() {
-		objModel.rotation().x++;
-		objModel.rotation().z++;
+		objModel.rotation().y++;
 	}
 }
