31d2ed1e8b2d8c7c8df1d3d8c638a610d3dec603
abdfairul <abdfairul@hotmail.com>
2015-05-30 13:11:31 +0800

    add more activities

--- sampleProjects/min3dSampleProject1/src/min3d/sampleProject1/Homepage.java
+++ sampleProjects/min3dSampleProject1/src/min3d/sampleProject1/Homepage.java
@@ -1,14 +1,16 @@
 package min3d.sampleProject1;
+import android.app.ListActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-
-import min3d.core.RendererActivity;
 
 /**
  * Created by fasannan on 29/5/2015.
  */
-public class Homepage extends RendererActivity {
+public class Homepage extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -16,6 +18,8 @@ public class Homepage extends RendererActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.homepage_layout);
+
+        registerForContextMenu(getListView());
     }
 
     public void sendMessage(View view) {
@@ -24,4 +28,42 @@ public class Homepage extends RendererActivity {
         Intent intent = new Intent(this, Step1.class);
         startActivity(intent);
     }
+
+
+    //
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        int i = 0;
+        menu.add(0, 0, i++, "Project Home");
+        menu.add(0, 1, i++, "Official Site");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent i;
+
+        switch (item.getItemId())
+        {
+            case 0:
+                i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(this.getResources().getString(R.string.projectUrl)));
+                startActivity(i);
+                return true;
+
+            case 1:
+                i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse( this.getResources().getString(R.string.siteUrl) ));
+                startActivity(i);
+                return true;
+        }
+        return false;
+    }
+
 }
