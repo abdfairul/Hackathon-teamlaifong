0e304eb9b14fd5b5364ad5d13ee073e3f82a6aa3
abdfairul <abdfairul@hotmail.com>
2015-05-30 10:57:18 +0800

    add more activities

--- sampleProjects/min3dSampleProject1/src/min3d/sampleProject1/LoadFaceAndFrame.java
+++ sampleProjects/min3dSampleProject1/src/min3d/sampleProject1/LoadFaceAndFrame.java
@@ -14,16 +14,21 @@ import min3d.vos.Light;
 /**
  * Created by fasannan on 30/5/2015.
  */
-public class LoadEyeWear extends RendererActivity{
+public class LoadFaceAndFrame extends RendererActivity {
+
     private Object3dContainer face;
+    private Object3dContainer spec;
+
+
+    private final float DEGREE = (float)(Math.PI/180);
 
     @Override
-    protected void onCreateSetContentView()
-    {
+    protected void onCreateSetContentView() {
         setContentView(R.layout.face_layout);
 
         Button mButton=(Button)findViewById(R.id.layoutOkay);
-        mButton.setText("COMBINE!");
+        mButton.setText("PRINT!");
+
 
         LinearLayout ll = (LinearLayout) this.findViewById(R.id.faceholder);
         ll.addView(_glSurfaceView);
@@ -32,7 +37,7 @@ public class LoadEyeWear extends RendererActivity{
     public void sendMessage(View view) {
         // Intent(context, class to send the intent to)
         // this Intent will call another activity (DisplayMesageActivity)
-        Intent intent = new Intent(this, LoadFaceAndFrame.class);
+        Intent intent = new Intent(this, Step2.class);
         startActivity(intent);
     }
 
@@ -40,23 +45,28 @@ public class LoadEyeWear extends RendererActivity{
     public void initScene() {
 
         scene.lights().add(new Light());
+        scene.lights().add(new Light());
+        Light myLight = new Light();
+        myLight.position.setZ(150);
+        myLight.ambient.setAll(0x88000011);
+        myLight.diffuse.setAll(0xff0000ff);
+        scene.lights().add(myLight);
 
         IParser parser = Parser.createParser(Parser.Type.OBJ,
-                getResources(), "min3d.sampleProject1:raw/glasses_001_obj", true);
+                getResources(), "min3d.sampleProject1:raw/face_obj", true);
         parser.parse();
 
         face = parser.getParsedObject();
         face.scale().x = face.scale().y = face.scale().z = .01f;
         scene.addChild(face);
 
+        IParser parser2 = Parser.createParser(Parser.Type.OBJ,
+                getResources(), "min3d.sampleProject1:raw/glasses_001_obj", true);
+        parser2.parse();
+
+        spec = parser2.getParsedObject();
+        spec.scale().x = spec.scale().y = spec.scale().z = .01f;
+        scene.addChild(spec);
+
     }
-
-    @Override
-    public void updateScene() {
-        face.rotation().y++;
-        face.rotation().x++;
-
-    }
-
-
 }
